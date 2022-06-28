f05ee9a0cb608f55f9067299fb95481521651c86
Llogari Casas <llogari92@gmail.com>
2022-06-28 15:52:17 -0700

    Initial push

---
+++ Maritimum/Maritimum/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Maritimum
+//
+//  Created by Llogari Casas on 28/6/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
